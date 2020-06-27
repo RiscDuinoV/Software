78f605a63593fbb77bb41cd6965948d0179a341e
Pedro CARVALHO MENDES <pedrocarvalho-12@hotmail.com>
2020-06-27 16:52:25 +0200

    i2c tested and working

--- system/src/i2c.c
+++ system/src/i2c.c
@@ -1,38 +1,32 @@
 #include <dev/i2c.h>
+static void s_i2c_waitTransmission(uint32_t base)
+{
+    int busy;
+    do
+    {
+        LB(busy, 1, base);
+    } 
+    while (busy & 0x1);
+}
 static int s_i2c_write(uint32_t base, uint8_t byte)
 {
     uint16_t tmp;
-    do
-    {
-        LB(tmp, 1, base);
-    } 
-    while ((tmp & 0x1) != 0); // Check if busy
+    s_i2c_waitTransmission(base);
     tmp = 0xA00 | byte;
     SH(tmp, 0, base);
-    do
-    {
-        LB(tmp, 1, base);
-    } 
-    while (tmp & 0x1);
-    return (tmp >> 1) & 0x1;   
+    s_i2c_waitTransmission(base);
+    LB(tmp, 1, base);
+    return (tmp >> 1) & 0x1;
 }
 static int s_i2c_read(uint32_t base, uint8_t *byte)
 {
     uint16_t tmp;
-    do
-    {
-        LB(tmp, 1, base);
-    } 
-    while ((tmp & 0x1) != 0);   // Check if busy
+    s_i2c_waitTransmission(base);
     SH(0x900, 0, base);       // Start Read
-    do
-    {
-        LB(tmp, 1, base);
-    } 
-    while (tmp & 0x1);
-    
-    LB(*byte, 0, base);
-    return (tmp >> 1) & 0x1;   
+    s_i2c_waitTransmission(base);
+    LH(tmp, 0, base);
+    *byte = tmp & 0xFF;
+    return (tmp >> 9) & 0x1;   
 }
 int i2c_write(uint32_t base, uint8_t address, const uint8_t *buffer, int len)
 {
@@ -51,7 +45,7 @@ int i2c_write(uint32_t base, uint8_t address, const uint8_t *buffer, int len)
     for (int i = 0; i < len; i++)
     {
         if (s_i2c_write(base, buffer[i]))
-            return -1;
+            break;
     }
     SH(0x400, 0, base);
     return 0;
@@ -73,7 +67,7 @@ int i2c_read(uint32_t base, uint8_t address, uint8_t *buffer, int len)
     for (int i = 0; i < len; i++)
     {
         if (s_i2c_read(base, &buffer[i]))
-            return -1;
+            break;
     }
     SH(0x400, 0, base);
     return 0;
