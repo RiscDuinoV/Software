ea867a2276b1b6d78755d825cc1d483c37f594dc
Pedro Carvalho Mendes <pedrocarvalho-12@hotmail.com>
2022-02-18 17:18:06 +0100

    sio gets function, tiny printf and print float

--- system/include/dev/sio.h
+++ system/include/dev/sio.h
@@ -12,6 +12,8 @@ int sio_print_number(uint32_t base, uint32_t n, int base_print);
 int sio_print_float(uint32_t base, float number, int digits);
 int sio_print_double(uint32_t base, double number, int digits);
 void sio_set_baud(uint32_t base, uint32_t baud);
+int sio_printf(uint32_t base, const char *format, ...);
+char *sio_gets(uint32_t base, char *str);
 #ifdef __cplusplus
 }
 #endif
--- system/src/sio.c
+++ system/src/sio.c
@@ -1,5 +1,6 @@
 #include "dev/io.h"
 #include "dev/sio.h"
+#include <stdarg.h>
 #define	SIO_TX_BUSY	0x4
 #define	SIO_RX_FULL	0x1
 int sio_available(uint32_t base)
@@ -59,7 +60,49 @@ int sio_print_number(uint32_t base, uint32_t n, int base_print)
 }
 int sio_print_float(uint32_t base, float number, int digits)
 {
-    return sio_print_double(base, number, digits);
+    int n = 0;
+    uint32_t *number32 = (uint32_t *)&number;
+    if (((*number32) & 0x7F800000) == 0x7F800000)
+    {
+        if (((*number32) & 0x007FFFFF) != 0)
+            sio_puts(base, "nan\n");
+        else if ((*number32) & 0x80000000)
+            sio_puts(base, "-inf\n");
+        else
+            sio_puts(base, "inf\n");
+    }
+    // Handle negative numbers
+    if (number < 0.0f)
+    {
+        n += sio_putchar(base, '-');
+        number = -number;
+    }
+
+    // Round correctly so that print(1.999, 2) prints as "2.00"
+    double rounding = 0.5;
+    for (int i = 0; i < digits; ++i)
+        rounding /= 10.0;
+
+    number += rounding;
+
+    // Extract the integer part of the number and print it
+    unsigned long int_part = (unsigned long)number;
+    float remainder = number - (float)int_part;
+    n += sio_print_number(base, int_part, 10);
+
+    // Print the decimal point, but only if there are digits beyond
+    if (digits > 0) 
+        n += sio_putchar(base, '.');
+
+    // Extract digits from the remainder one at a time
+    while (digits-- > 0)
+    {
+        remainder *= 10.0f;
+        unsigned int toPrint = (unsigned int)(remainder);
+        n += sio_print_number(base, toPrint, 10);
+        remainder -= toPrint;
+    }
+    return n;
 }
 int sio_print_double(uint32_t base, double number, int digits)
 {
@@ -105,6 +148,68 @@ int sio_print_double(uint32_t base, double number, int digits)
     }
     return n;
 }
+char *sio_gets(uint32_t base, char *str)
+{
+    int c;
+    while ((c = sio_getch(base)) != '\n')
+    {
+        *str = c;
+        str++;
+    }
+    return str;
+}
+int sio_printf(uint32_t base, const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    int c;
+    int ret = 0;
+    while ((c = *format++) != 0)
+    {
+        ret++;
+        if ((c == '%') && ((c = *format++) != 0))
+        {
+            ret++;
+            if (c == 'c')
+            {
+                int v = va_arg(args, int);
+                sio_putchar(base, v);
+            }
+            else if (c == 'd')
+            {
+                int v = va_arg(args, int);
+                sio_print_number(base, v, 10);
+            }
+            else if (c == 'x')
+            {
+                uint32_t v = va_arg(args, uint32_t);
+                sio_print_number(base, v, 16);
+            }
+            else if (c == 's')
+            {
+                const char *str = va_arg(args, const char *);
+                sio_puts(base, str);
+            }
+            #ifdef SIO_PRINTF_FLOAT
+            else if (c == 'f')
+            {
+                double f = va_arg(args, double);
+                sio_print_float(base, f, 16);
+            }
+            #endif
+            else
+            {
+                ret--;
+            }
+        }
+        else 
+        {
+            sio_putchar(base, c);
+        }
+    }
+    va_end(args);
+    return ret;
+}
 #define X 262144
 #define Y 64
 void sio_set_baud(uint32_t base, uint32_t baud)
@@ -117,6 +222,9 @@ void sio_set_baud(uint32_t base, uint32_t baud)
 }
 __attribute__((weak)) int __io_putchar(int ch)
 {
-    sio_putchar(UART_NUM(0), ch);
-    return 0;
+    return sio_putchar(UART_NUM(0), ch);
+}
+__attribute__((weak)) int __io_getchar()
+{    
+    return sio_getch(UART_NUM(0));
 }
